7399acb2cd0dc9602373f848e917a0ac007ad7a1
zfree <79763939@qq.com>
2019-10-05 23:25:53 +0800

    加入了think-swagger-ui模块

---
+++ think-swagger-ui-starter/src/main/java/com/tennetcn/free/swagger/autoconfig/SwaggerUIAutoConfiguation.java
@@ -0,0 +1,13 @@
+package com.tennetcn.free.swagger.autoconfig;
+
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author chfree
+ * @email chfree001@gmail.com
+ * @create 2019-10-05 21:30
+ * @comment
+ */
+@ImportResource(locations={"swaggerui-starter-config.xml"})
+public class SwaggerUIAutoConfiguation {
+}
