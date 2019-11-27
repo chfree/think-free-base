b7282e41530c7ef61890282b57ea4fe6c96317f0
zfree <79763939@qq.com>
2019-11-27 12:31:34 +0800

    think-swagger-ui ccbft特供版本

--- think-swagger-ui-starter/src/main/java/com/tennetcn/free/swagger/autoconfig/SwaggerUIAutoConfiguation.java
+++ think-swagger-ui-starter/src/main/java/com/tennetcn/free/swagger/autoconfig/SwaggerUIAutoConfiguation.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.ImportResource;
  * @create 2019-10-05 21:30
  * @comment
  */
-@ImportResource(locations={"swaggerui-starter-config.xml"})
+@ImportResource(locations={"classpath*:think-swaggerui-starter-config.xml"})
 public class SwaggerUIAutoConfiguation {
 }
---
+++ think-swagger-ui-starter/src/main/java/com/tennetcn/free/swagger/autoconfig/ThinkSwaggerWebMvcConfigurerAdapter.java
@@ -0,0 +1,22 @@
+package com.tennetcn.free.swagger.autoconfig;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author chfree
+ * @email chfree001@gmail.com
+ * @create 2019-10-05 22:21
+ * @comment
+ */
+
+@Component
+public class ThinkSwaggerWebMvcConfigurerAdapter implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/thinkswagger/**").addResourceLocations("/WEB-INF/thinkswagger/");
+
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
