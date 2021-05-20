f3e660470cc9850630c10d30aaa40c4ce0a10092
chenghuan-home <123256085@qq.com>
2021-05-20 23:17:27 +0800

    创建项目的界面初始化处理

---
+++ web-starter/src/main/java/com/tennetcn/free/web/newproject/apis/NewProjectApi.java
@@ -0,0 +1,15 @@
+package com.tennetcn.free.web.newproject.apis;
+
+import com.tennetcn.free.web.webapi.FirstApi;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/newproject")
+public class NewProjectApi extends FirstApi {
+    @GetMapping(value = "/index")
+    public String hello(){
+        return "/newproject/index.html";
+    }
+}
