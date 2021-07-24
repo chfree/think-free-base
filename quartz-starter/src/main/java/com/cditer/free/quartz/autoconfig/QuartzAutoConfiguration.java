8aa993b79b4221a49817839d695fb29f110ab8cb
chenghuan-home <123256085@qq.com>
2021-07-24 23:21:39 +0800

    修改xml中的路径

--- quartz-starter/src/main/java/com/cditer/free/quartz/autoconfig/QuartzAutoConfiguration.java
+++ quartz-starter/src/main/java/com/cditer/free/quartz/autoconfig/QuartzAutoConfiguration.java
@@ -3,7 +3,7 @@ package com.cditer.free.quartz.autoconfig;
 import org.springframework.context.annotation.ImportResource;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@MapperScan(value = "com.tennetcn.free.quartz.logical.mapper")
+@MapperScan(value = "com.cditer.free.quartz.logical.mapper")
 @ImportResource(locations={"classpath*:quartz-spring-boot-config.xml"})
 public class QuartzAutoConfiguration {
 
