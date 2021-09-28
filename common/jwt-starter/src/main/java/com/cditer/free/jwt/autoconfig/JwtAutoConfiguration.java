becc5ffe6b9da0b893d24ab36f55a11503949a3a
zfree <79763939@qq.com>
2021-09-28 21:52:30 +0800

    jwt-util重命名为jwt-starter

---
+++ common/jwt-starter/src/main/java/com/cditer/free/jwt/autoconfig/JwtAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.cditer.free.jwt.autoconfig;
+
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author chfree
+ * @email chfree001@gmail.com
+ * @create 2021-09-28 21:31
+ * @comment
+ */
+
+@ImportResource(locations={"classpath:jwt-spring-boot-config.xml"})
+public class JwtAutoConfiguration {
+}
