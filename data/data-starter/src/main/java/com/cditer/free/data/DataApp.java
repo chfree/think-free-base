6b8a9670260eea4d76bd46819f6e5c9cd4fe373e
zfree <79763939@qq.com>
2021-10-13 20:27:57 +0800

    data-starter的测试配置

---
+++ data/data-starter/src/main/java/com/cditer/free/data/DataApp.java
@@ -0,0 +1,20 @@
+package com.cditer.free.data;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class DataApp implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("DataTestApp is runing");
+    }
+}
---
+++ data/data-starter/src/test/java/com/cditer/free/data/test/mapper/ITestDataUserMapper.java
@@ -0,0 +1,2 @@
+package com.cditer.free.data.test.mapper;public class ITestDataUserMapper {
+}
