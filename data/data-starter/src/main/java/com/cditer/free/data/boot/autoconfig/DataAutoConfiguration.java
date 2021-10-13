5aa76f056de51cbfdb1faa81166ee27f955d1cdc
chenghuan-home <123256085@qq.com>
2021-10-13 23:44:13 +0800

    test:data-starter测试

--- data/data-starter/src/main/java/com/cditer/free/data/boot/autoconfig/DataAutoConfiguration.java
+++ data/data-starter/src/main/java/com/cditer/free/data/boot/autoconfig/DataAutoConfiguration.java
@@ -1,10 +1,8 @@
 package com.cditer.free.data.boot.autoconfig;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-@ImportResource(locations={"classpath:data-spring-boot-config.xml"})
+@ImportResource(locations={"classpath*:data-spring-boot-config.xml"})
 public class DataAutoConfiguration {
 	
 }
