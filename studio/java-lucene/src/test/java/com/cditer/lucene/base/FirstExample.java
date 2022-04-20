5de6428acee093ce9b7fe467ad37064eeef2d374
chfree <79763939@qq.com>
2022-04-20 23:27:41 +0800

    lucene分词处理

--- studio/java-lucene/src/test/java/com/cditer/lucene/base/FirstExample.java
+++ studio/java-lucene/src/test/java/com/cditer/lucene/base/FirstExample.java
@@ -1,6 +1,7 @@
 package com.cditer.lucene.base;
 
 import cn.hutool.json.JSONUtil;
+import com.cditer.free.core.exception.BizException;
 import com.cditer.lucene.data.ArticleDataFactory;
 import com.cditer.lucene.model.Article;
 import lombok.extern.slf4j.Slf4j;
@@ -9,26 +10,120 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.wltea.analyzer.lucene.IKSynonymAnalyzer;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class FirstExample {
 
-    public void queryArticleTest(){
+    private static final String path = "E:\\document\\testfile\\lucene\\test\\article";
 
+    @Test
+    public void queryArticleTest() {
+        BooleanQuery.Builder builder = new BooleanQuery.Builder();
+        // 精确查找
+        TermQuery termQuery = new TermQuery(new Term("id", "001"));
+        builder.add(termQuery, BooleanClause.Occur.MUST);
+        // 接一个模糊查找
+        booleanQueryAddNotEmpty(builder, "title", "java JReleaser");
+
+        List<Article> articles = getArticle(builder.build());
+
+        Assert.assertTrue(articles.size()==1);
+        Assert.assertEquals("001", articles.get(0).getId());
     }
 
+    private List<Article> getArticle(Query query) {
+        IndexReader indexReader = null;
+        Directory directory = null;
+        List<Article> resultList = new ArrayList<>();
+        try {
+            directory = FSDirectory.open(Paths.get(path));
+            indexReader = DirectoryReader.open(directory);
+
+            IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+            indexSearcher.setSimilarity(new ClassicSimilarity());
+
+            Sort sort = Sort.RELEVANCE;
+            ScoreDoc[] docs = indexSearcher.search(query, 10, sort, true).scoreDocs;
+
+            for (ScoreDoc doc : docs) {
+                Document document = indexSearcher.doc(doc.doc);
+                Article dto = getLuceneModel(document);
+
+                resultList.add(dto);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (indexReader != null) {
+                try {
+                    indexReader.close();
+                } catch (Exception e) {
+                    log.error("关闭indexReader发生异常", e);
+                }
+            }
+            if (directory != null) {
+                try {
+                    directory.close();
+                } catch (Exception e) {
+                    log.error("模糊搜索发生异常：", e);
+                }
+            }
+        }
+
+        return resultList;
+    }
+
+
+    private Article getLuceneModel(Document document) throws IOException {
+        Article dto = new Article();
+
+        dto.setId(document.get("id"));
+        dto.setSummary(document.get("summary"));
+        dto.setContent(document.get("content"));
+        dto.setTitle(document.get("title"));
+        dto.setSrc(document.get("src"));
+        dto.setStatus(document.get("status"));
+//        dto.setTag(Arrays.asList(document.get("tag").split(",")));
+
+        return dto;
+    }
+
+    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder,String keyName,String keyVal) {
+        booleanQueryAddNotEmpty(builder, keyName, keyVal, BooleanClause.Occur.MUST);
+    }
+
+    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder,String keyName,String keyVal,BooleanClause.Occur occur)  {
+        if(!StringUtils.hasText(keyVal)){
+            return;
+        }
+        try {
+            builder.add(buildQuery(keyName, keyVal), occur);
+        }catch (ParseException ex){
+            throw new BizException("booleanQueryAddNotEmpty ParseException", ex);
+        }
+    }
+
+    private Query buildQuery(String keyName,String keyVal) throws ParseException {
+        return (new QueryParser(keyName, new IKSynonymAnalyzer())).parse(keyVal);
+    }
 
     /**
      * 创建全量索引测试
@@ -40,7 +135,6 @@ public class FirstExample {
         Directory directory = null;
 
         try {
-            String path = "E:\\document\\testfile\\lucene\\test\\article";
 
             File file = new File(path);
             if (!file.exists()) {
@@ -90,6 +184,8 @@ public class FirstExample {
             document.add(new StringField("id", article.getId(), Field.Store.YES));
             document.add(new TextField("summary", article.getSummary(), Field.Store.YES));
             document.add(new StringField("status", article.getStatus(), Field.Store.YES));
+            document.add(new StringField("src", article.getSrc(), Field.Store.YES));
+            document.add(new StringField("tag", String.join(",", article.getTag()), Field.Store.YES));
 
             try {
                 indexWriter.addDocument(document);
