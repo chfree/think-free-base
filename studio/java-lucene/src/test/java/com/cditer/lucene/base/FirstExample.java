c1bfdcfae0a05af55696b3c9b8f7ed740086da21
chfree <79763939@qq.com>
2022-04-21 11:09:09 +0800

    测试lucene

--- studio/java-lucene/src/test/java/com/cditer/lucene/base/FirstExample.java
+++ studio/java-lucene/src/test/java/com/cditer/lucene/base/FirstExample.java
@@ -1,5 +1,6 @@
 package com.cditer.lucene.base;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.json.JSONUtil;
 import com.cditer.free.core.exception.BizException;
 import com.cditer.lucene.data.ArticleDataFactory;
@@ -27,7 +28,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class FirstExample {
@@ -35,7 +38,7 @@ public class FirstExample {
     private static final String path = "E:\\document\\testfile\\lucene\\test\\article";
 
     @Test
-    public void queryArticleTest() {
+    public void queryArticleBoolQueryTest() {
         BooleanQuery.Builder builder = new BooleanQuery.Builder();
         // 精确查找
         TermQuery termQuery = new TermQuery(new Term("id", "001"));
@@ -45,8 +48,47 @@ public class FirstExample {
 
         List<Article> articles = getArticle(builder.build());
 
-        Assert.assertTrue(articles.size()==1);
+        Assert.assertTrue(articles.size() == 1);
         Assert.assertEquals("001", articles.get(0).getId());
+    }
+
+    @Test
+    public void queryArticleTermTest() {
+        // 精确查找
+        TermQuery termQuery = new TermQuery(new Term("id", "001"));
+        List<Article> articles = getArticle(termQuery);
+
+        Assert.assertTrue(articles.size() == 1);
+        Assert.assertEquals("001", articles.get(0).getId());
+
+        termQuery = new TermQuery(new Term("id", "a001"));
+        articles = getArticle(termQuery);
+
+        Assert.assertTrue(CollectionUtils.isEmpty(articles));
+    }
+
+    @Test
+    public void queryArticleInTest() {
+        // in查找
+        BooleanQuery.Builder builder = new BooleanQuery.Builder();
+        booleanQueryAddNotEmpty(builder, "id", "001", BooleanClause.Occur.SHOULD);
+        booleanQueryAddNotEmpty(builder, "id", "004", BooleanClause.Occur.SHOULD);
+
+        List<Article> articles = getArticle(builder.build());
+
+        Assert.assertTrue(articles.size() == 2);
+        Assert.assertEquals("001", articles.get(0).getId());
+
+
+        builder = new BooleanQuery.Builder();
+        booleanQueryAddNotEmpty(builder, "id", "001", BooleanClause.Occur.SHOULD);
+        booleanQueryAddNotEmpty(builder, "id", "04", BooleanClause.Occur.SHOULD);
+
+        articles = getArticle(builder.build());
+
+        Assert.assertTrue(articles.size() == 1);
+        Assert.assertEquals("001", articles.get(0).getId());
+
     }
 
     private List<Article> getArticle(Query query) {
@@ -71,7 +113,7 @@ public class FirstExample {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (indexReader != null) {
                 try {
                     indexReader.close();
@@ -106,23 +148,101 @@ public class FirstExample {
         return dto;
     }
 
-    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder,String keyName,String keyVal) {
+    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder, String keyName, String keyVal) {
         booleanQueryAddNotEmpty(builder, keyName, keyVal, BooleanClause.Occur.MUST);
     }
 
-    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder,String keyName,String keyVal,BooleanClause.Occur occur)  {
-        if(!StringUtils.hasText(keyVal)){
+    private void booleanQueryAddNotEmpty(BooleanQuery.Builder builder, String keyName, String keyVal, BooleanClause.Occur occur) {
+        if (!StringUtils.hasText(keyVal)) {
             return;
         }
         try {
             builder.add(buildQuery(keyName, keyVal), occur);
-        }catch (ParseException ex){
+        } catch (ParseException ex) {
             throw new BizException("booleanQueryAddNotEmpty ParseException", ex);
         }
     }
 
-    private Query buildQuery(String keyName,String keyVal) throws ParseException {
+    private Query buildQuery(String keyName, String keyVal) throws ParseException {
         return (new QueryParser(keyName, new IKSynonymAnalyzer())).parse(keyVal);
+    }
+
+    @Test
+    public void addOrUpdateCheckTest(){
+        IndexReader indexReader = null;
+        Directory directory = null;
+        try {
+            directory = FSDirectory.open(Paths.get(path));
+            indexReader = DirectoryReader.open(directory);
+
+            System.out.println(indexReader.maxDoc());
+            System.out.println(indexReader.numDeletedDocs());
+            System.out.println(indexReader.numDocs());
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (indexReader != null) {
+                try {
+                    indexReader.close();
+                } catch (Exception e) {
+                    log.error("关闭indexReader发生异常", e);
+                }
+            }
+            if (directory != null) {
+                try {
+                    directory.close();
+                } catch (Exception e) {
+                    log.error("模糊搜索发生异常：", e);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void addIndexOrDbTest() {
+        IndexWriter iwriter = null;
+        Directory directory = null;
+
+        Article article = new Article();
+        article.setId("a001");
+        article.setTitle(String.format("测试001,%s", DateUtil.now()));
+        article.setSummary(String.format("这是一个测试001,%s", DateUtil.now()));
+        article.setContent("测试content");
+        article.setTag(Arrays.asList("测试,content".split(",")));
+        article.setStatus("03");
+        article.setSrc("test");
+
+        try {
+
+            File file = new File(path);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+
+            Analyzer analyzer = new IKSynonymAnalyzer();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            directory = FSDirectory.open(Paths.get(path));
+            iwriter = new IndexWriter(directory, config);
+
+            createIndexDb(iwriter, Arrays.asList(article), "add");
+        } catch (IOException e) {
+            log.error("创建索引发生异常：", e);
+        } finally {
+            try {
+                if (iwriter != null) {
+                    iwriter.close();
+                }
+                if (directory != null) {
+                    directory.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -149,7 +269,7 @@ public class FirstExample {
 
             iwriter.deleteAll();
 
-            createIndexDb(iwriter, list);
+            createIndexDb(iwriter, list, "all");
         } catch (IOException e) {
             log.error("创建索引发生异常：", e);
         } finally {
@@ -168,9 +288,19 @@ public class FirstExample {
     }
 
 
-    private void createIndexDb(IndexWriter indexWriter, List<Article> list) {
+    private void createIndexDb(IndexWriter indexWriter, List<Article> list, String fun) {
         if (CollectionUtils.isEmpty(list)) {
             return;
+        }
+        if ("add".equals(fun)) {
+            List<Query> querys = list.stream().map(item -> {
+                return (new TermQuery(new Term("id", item.getId())));
+            }).filter(item -> item != null).collect(Collectors.toList());
+            try {
+                indexWriter.deleteDocuments(querys.toArray(new Query[querys.size()]));
+            } catch (Exception ex) {
+                log.error("删除索引文件出错");
+            }
         }
         log.info("即将本地索引的数据为:");
         log.info(JSONUtil.toJsonStr(list));
