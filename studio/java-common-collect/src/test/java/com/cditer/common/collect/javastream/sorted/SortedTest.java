94ea373e0e979638cb6120c4d5d78cb9e81259f0
chfree <79763939@qq.com>
2022-08-12 00:06:49 +0800

    排倒序示例完善

--- studio/java-common-collect/src/test/java/com/cditer/common/collect/javastream/sorted/SortedTest.java
+++ studio/java-common-collect/src/test/java/com/cditer/common/collect/javastream/sorted/SortedTest.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class SortedTest {
 
     @Test
-    public void sorted(){
+    public void sorted() {
         List<Integer> list = new ArrayList<Integer>();
         list.add(8);
         list.add(19);
@@ -29,19 +29,41 @@ public class SortedTest {
         List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
 
         for (int i = 0; i < sortedList.size(); i++) {
-            if(i<sortedList.size()-1) {
+            if (i < sortedList.size() - 1) {
                 Assert.assertTrue(sortedList.get(i) < sortedList.get(i + 1));
             }
         }
 
         List<String> sortedStrList = sortedList.stream().map(item -> String.valueOf(item)).collect(Collectors.toList());
 
-        Assert.assertEquals(String.join(",", sortedStrList),"-8,2,8,19");
+        Assert.assertEquals(String.join(",", sortedStrList), "-8,2,8,19");
 
     }
 
     @Test
-    public void testModelSorted(){
+    public void sortedDesc() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(8);
+        list.add(19);
+        list.add(2);
+        list.add(-8);
+
+        List<Integer> sortedList = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        for (int i = 0; i < sortedList.size(); i++) {
+            if (i < sortedList.size() - 1) {
+                Assert.assertTrue(sortedList.get(i) > sortedList.get(i + 1));
+            }
+        }
+
+        List<String> sortedStrList = sortedList.stream().map(item -> String.valueOf(item)).collect(Collectors.toList());
+
+        Assert.assertEquals(String.join(",", sortedStrList), "19,8,2,-8");
+
+    }
+
+    @Test
+    public void testModelSorted() {
         List<User> users = new ArrayList<>();
         users.add(new User("cheng", 18));
         users.add(new User("chfree", 16));
@@ -54,5 +76,17 @@ public class SortedTest {
         Assert.assertEquals("16,18,20", String.join(",", ages));
     }
 
+    @Test
+    public void testModelSortedDesc() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("cheng", 18));
+        users.add(new User("chfree", 16));
+        users.add(new User("ch", 20));
 
+        List<User> sortedUsers = users.stream().sorted(Comparator.comparing(item -> item.getAge(), Comparator.reverseOrder())).collect(Collectors.toList());
+
+        List<String> ages = sortedUsers.stream().map(item -> String.valueOf(item.getAge())).collect(Collectors.toList());
+
+        Assert.assertEquals("20,18,16", String.join(",", ages));
+    }
 }
